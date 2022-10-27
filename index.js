afff7bd29f14fe117064c68526f6549eb20dc260
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-10-26 19:13:06 -0500

    Actualizacion, operadores decrecientes y crecientes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 let precioBitcoin = 67344;
 let precioEthereum = 4752;
 let precioDogecoin = 0.2749;
-
 //utiliza el operador ++ para aumentar precioBitcoin por 1
-
+precioBitcoin++;
 //utiliza el operador -- para disminuir el precioEthereum por 1
-
+precioEthereum--;
 //utiliza el operador += para aumentar el precioDogecoin por 0.50
+precioDogecoin+= 0.50
